ef95682b32701365344362fd6c0b96fbf451df38
Reuben Salagaras <reuben.salagaras@gmail.com>
2013-03-30 14:23:40 +1030

    put tinymce theme in wrong location. corrected

--- config/deploy.rb
+++ config/deploy.rb
@@ -3,7 +3,7 @@
 # Fat Free CRM is freely distributable under the terms of MIT license.
 # See MIT-LICENSE file or http://www.opensource.org/licenses/mit-license.php
 #------------------------------------------------------------------------------
-$:.unshift File.expand_path('./lib', ENV['rvm_path'])
+#$:.unshift File.expand_path('./lib', ENV['rvm_path'])
 
 require 'rvm/capistrano'
 require 'bundler/capistrano'
