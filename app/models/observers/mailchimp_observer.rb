14511890f00d12dc4d4e324edd47c31e263ed360
Reuben Salagaras <reuben.salagaras@gmail.com>
2013-03-31 16:58:20 +1030

    Mailchimp sync code update.
    - triggers update on gender change
    - fixed bug where changing email address and newly subscribing to a mailing list in the same edit would subscribe the contact with their *old* email address

--- app/models/observers/mailchimp_observer.rb
+++ app/models/observers/mailchimp_observer.rb
@@ -12,7 +12,7 @@ class MailchimpObserver < ActiveRecord::Observer
   end
 
   def after_update(contact)
-    if contact.cf_weekly_emails_changed? || contact.email_changed? || contact.first_name_changed? || contact.last_name_changed?
+    if (contact.changes.keys & %w(cf_weekly_emails email first_name last_name cf_gender)).any?
       mailchimp_lists(contact)
     end
   end
@@ -29,14 +29,24 @@ class MailchimpObserver < ActiveRecord::Observer
   def mailchimp_lists(contact)
     unsubscribed_weekly = contact.cf_weekly_emails_was - contact.cf_weekly_emails # ["Adelaide", "City East"] - ["Adelaide"] => ["City East"]
     
-    if contact.email_changed? || contact.first_name_changed? || contact.last_name_changed?
+    if (contact.changes.keys & %w(email first_name last_name cf_gender)).any?
       subscribed_weekly = contact.cf_weekly_emails #add/update all
     else
       subscribed_weekly = contact.cf_weekly_emails - contact.cf_weekly_emails_was #just add to new lists
     end
     
+    # email_was is passed to add_or_update_chimp as a key to search in mailchimp lists. Therefore
+    # update code needs to know what the email address was to find the contact in mailchimp lists.
+    #
+    # The two situations where we don't want to use previous email address are:
+    # 
+    # 1) previous email was blank (this contact wouldn't be on any mailchimp lists - it's invalid)
+    # 2) contact was previously not subscribed to any lists, so no need to search by old email address
+    #
+    # the search key should probably be changed to a uid stored as an attribute of the contact
+     
     email_was = contact.email_changed? ? contact.email_was : nil
-    email_was = nil if contact.email_was.blank?
+    email_was = nil if contact.email_was.blank? || contact.cf_weekly_emails_was.reject(&:empty?).blank?
     
     unsubscribed_weekly.reject(&:blank?).each do |list|
       self.delay.delete_chimp(contact, list.gsub(/\s+/, "").underscore, email_was)
